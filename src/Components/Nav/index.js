54965212002059edb62afcfe378b3815a02a9824
Jr Ugalde <enrilougalde@gmail.com>
2024-02-21 20:39:08 -0800

    Updated Nav bar Css

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./style.css";
+// import Container from "react-bootstrap/Container";
 // import Nav from "react-bootstrap/Nav";
 // import Navbar from "react-bootstrap/Navbar";
+// import NavDropdown from "react-bootstrap/NavDropdown";
 
 export default function App() {
   function findPos(obj) {
@@ -22,17 +24,29 @@ export default function App() {
     });
   }
   return (
-    // <Nav className="justify-content-end" activeKey="/home">
-    // <Nav.Item>
-    //   <Nav.Link href="/home">Active</Nav.Link>
-    // </Nav.Item>
-    // <Nav.Item>
-    //   <Nav.Link eventKey="link-1">Link</Nav.Link>
-    // </Nav.Item>
-    // <Nav.Item>
-    //   <Nav.Link eventKey="link-2">Link</Nav.Link>
-    // </Nav.Item>
-    // </Nav>
+    // <Navbar expand="lg" className="bg-body-tertiary">
+    //   <Container>
+    //     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //       <Nav className="me-auto">
+    //         <Nav.Link href="#home">Home</Nav.Link>
+    //         <Nav.Link href="#link">Link</Nav.Link>
+    //         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+    //           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+    //           <NavDropdown.Item href="#action/3.2">
+    //             Another action
+    //           </NavDropdown.Item>
+    //           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+    //           <NavDropdown.Divider />
+    //           <NavDropdown.Item href="#action/3.4">
+    //             Separated link
+    //           </NavDropdown.Item>
+    //         </NavDropdown>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
 
     <nav className="navbar fixed-top navbar-expand-lg navbar-dark">
       <button
@@ -43,21 +57,26 @@ export default function App() {
         JR
       </button>
       <button
-        className="navbar-toggler "
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+      <div className="collapse navbar-collapse" id="navbarNav">
         <div className="navbar-nav ml-auto">
-          <button  className="nav-link js-scroll-trigger"
+          <button
+            className="nav-link js-scroll-trigger"
             onClick={() => scrollToSection("About")}
-            style={{ cursor: "default" }}>ABOUT</button>
+            style={{ cursor: "default" }}
+          >
+            ABOUT
+          </button>
 
           <button
             className="nav-link  js-scroll-trigger"
