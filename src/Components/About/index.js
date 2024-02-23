a8276375adc745714c19116c2c4a03920580ea6b
Jr Ugalde <enrilougalde@gmail.com>
2024-02-22 23:20:37 -0800

    Updated About

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -18,7 +18,7 @@ export default function About() {
           <h1>Enrilo M. Ugalde JR</h1>
           <hr></hr>
           <div className="content">
-            <b>Location: </b>San Diego and Los Angeles | <b>Phone# </b>{" "}
+            <b>Location: </b>San Diego  | <b>Phone# </b>
             619-961-5190 | <b>EMAIL: </b> ENRILOUGALDE@GMAIL.COM
             <br />
             <div className="row">
@@ -26,29 +26,43 @@ export default function About() {
                 <img
                   src={Profile}
                   alt="Enrilo Ugalde Profile"
-                  className="float-left mr-3"
+                  className="float-left mr-1 ml-4"
                 />
-                A computer geek at heart, my interest in the world of technology
-                began when my parents introduced me to my first computer, MSDOS.
-                Since then I have been fixing, repairing, dissecting the
-                technical side of a personal computer. As my 15 year mark
-                working in administration for the Government comes, my wife
-                sparked an interest in the web development side of technology.
-                Although we are in an environment where we are secluded, I am
-                excited to work with a creative team that will make the world a
-                better place one code, script, or gesture at a time. Today, I am
-                on path to become a Full Stack: Developer in which I will be
-                proficient in Front End:
+                <b>
+                  My journey from an Electronics Store Sales Associate to a
+                  Software Engineer at Booz Allen Hamilton has been one of
+                  dedication, perseverance, and continuous learning. Graduating
+                  from the UCSD Extension Coding Bootcamp was a pivotal moment,
+                  marking the beginning of my transition into the tech industry.
+                  As a Sales Associate, I honed my customer service and
+                  communication skills, which have proven invaluable in my
+                  current role. My time at the bootcamp equipped me with a
+                  strong foundation in programming languages, software
+                  development methodologies, and problem-solving skills, setting
+                  the stage for my career as a Software Engineer. Joining Booz
+                  Allen Hamilton has been an exciting opportunity to apply my
+                  technical expertise to real-world challenges and contribute to
+                  innovative solutions. I am passionate about technology and
+                  eager to continue growing in my technical skills. My journey
+                  is a testament to my commitment to personal and professional
+                  development, and I look forward to the opportunities that lie
+                  ahead.
+                </b>
+                <br></br>
+                <b>Front End:</b>
+                <br></br>
                 <img src={Html} alt="HTML" />,
                 <img src={csIcon} alt="CSS" />,
                 <img src={Jscript} alt="Javascrtip" />,
                 <img src={Jquery} alt="JQUERY" />,
-                <img src={React} alt="REACT" /> and Back End:{" "}
+                <img src={React} alt="REACT" />
+                <br></br>
+                <b>Back End:</b>
+                <br></br>
                 <img src={NodeJs} alt="NodeJS" />,
                 <img src={Express} alt="Express" />,
-                <img src={MySql} alt="MYSQL" />,{" "}
-                <img src={MongoDB} alt="MONGODB" />. and many more evolving
-                languages in this field.
+                <img src={MySql} alt="MYSQL" />,
+                <img src={MongoDB} alt="MONGODB" />.
                 <br />
                 <br />
                 <b>
